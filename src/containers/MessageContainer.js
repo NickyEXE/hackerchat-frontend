b554c1590bc754979432673d9446bc97d3d7e32e
NickyEXE <dovern42@gmail.com>
2020-11-12 12:33:30 -0500

    end of lecture 1

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -6,35 +6,33 @@ import { v4 as uuidv4 } from 'uuid';
 class MessageContainer extends React.Component {
 
   state = {
-    messages: [
-      {
-        id: 1,
-        username: "1NiteAtFreddys",
-        content: "Starting a new movie where I kill a bunch of coding school students. Calling it Freddy vs. JSON"
-      },
-      {
-        id: 2,
-        username: "NotThatMikeMyers",
-        content: "Freddy curses too much for my taste - Michael Myers"
-      },
-      {
-        id: 3,
-        username: "1NiteAtFreddys",
-        content: "Honestly I've found everyone's nightmares are bad enough without my help today. -FK"
-      }
-    ]
+    messages: []
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/messages")
+    .then(res => res.json())
+    .then(json => this.setState({ messages: json }))
   }
 
   addMessage = (message) => {
-    const newMessage = {...message, id: uuidv4()}
-    this.setState({messages: [...this.state.messages, newMessage]})
+    // const newMessage = {...message, id: uuidv4()}
+    fetch('http://localhost:3000/messages', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    })
+    .then(response => response.json())
+    .then(newMessage => this.setState({messages: [...this.state.messages, newMessage]}))
   }
 
   render(){
     return(
       <>
         <MessageForm addMessage={this.addMessage}/>
-        <MessageList messages={this.state.messages} cheese="squirrel"/>
+        <MessageList messages={this.state.messages}/>
       </>
     )
   }
